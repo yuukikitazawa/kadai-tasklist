db01c8eff0f91a643138d6546e05a37b02833e3f
yuuki.kitazawa <northyk0314@gmail.com>
2023-01-05 18:59:28 +0900

    jspファイルの移動

---
+++ WebContent/WEB-INF/views/tasks/_form.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+
+
+<label for="content_msg">メッセージ</label><br />
+<input type="text" name="content" id="content_msg" value="${message.content}" />
+<br /><br />
+
+<input type="hidden" name="_token" value="${_token}" />
+<button type="submit">投稿</button>
--- src/controllers/IndexServlet.java
+++ src/controllers/IndexServlet.java
@@ -35,14 +35,33 @@ public class IndexServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         EntityManager em = DBUtil.createEntityManager();
 
-        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class).getResultList();
+        int page = 1;
+        try {
+            page = Integer.parseInt(request.getParameter("page"));
+        } catch(NumberFormatException e) {}
+
+        List<Task> tasks = em.createNamedQuery("getAllTasks", Task.class)
+        .setFirstResult(15 * (page - 1))
+        .setMaxResults(15)
+        .getResultList();
+
+        long tasks_count = (long)em.createNamedQuery("getTasksCount", Long.class)
+                .getSingleResult();
 
         em.close();
 
         request.setAttribute("Tasks", tasks);
+        request.setAttribute("tasks_count", tasks_count);     // 全件数
+        request.setAttribute("page", page);
 
-        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/messages/index.jsp");
+        if(request.getSession().getAttribute("flush") != null) {
+            request.setAttribute("flush", request.getSession().getAttribute("flush"));
+            request.getSession().removeAttribute("flush");
+        }
+
+        RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/tasks/index.jsp");
         rd.forward(request, response);
     }
+
 
 }
